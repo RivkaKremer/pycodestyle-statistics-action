a6e55708d9d47376a7e59e6fa70f968130ed7559
Rivka Kremer <rivka.k@develeap.com>
2019-12-23 10:25:13 +0200

    start

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,16 +14,11 @@ SUCCESS=$?
 echo $OUTPUT
 set -e
 
-if [[ -z "$PRECOMMAND_MESSAGE" ]]; then
-    echo "No precommand message"
-else
-    echo "There is a precommand message"
-    OUTPUT=$PRECOMMAND_MESSAGE$'\n\n'$OUTPUT
-fi
 
-# If there were errors as part of linting, post a comment. Else, do nothing.
+
+
 if [ $SUCCESS -ne 0 ]; then
-    if [ ! -z "$PRECOMMAND_MESSAGE" ]; then
+    if [[ ! -z "$PRECOMMAND_MESSAGE" ]]; then
         OUTPUT=$PRECOMMAND_MESSAGE'\n'$OUTPUT
     fi
 else
